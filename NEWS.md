04478438e93ee7760e38498a08207527ea86f776
Dan Schafer <dschafer@meta.com>
2023-04-30 20:24:27 -0700

    Switch from timetk to slider for sliding window
    `timetk` got archived from CRAN at one point, causing this package to get archived as well.
    
    While `timetk` is back up, we only use it for a single function, and that function is basically a re-export from the lower-level `slider` package.
    
    So this removes our dependency on `timetk` and replaces it with a dependency on `slider` instead.
    
    As it turns out, this also gives us better results, since we're able to more clearly note that we're looking for a sliding window on the "time" column of 30 seconds. So the test is updated, and the results look more correct.
    
    Resolves #2

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,7 @@
 # activatr (development version)
 
+* Switch dependency from `timetk` to `slider` to avoid unnecessary indirection (#3).
+
 # activatr 0.1.0
 
 * Initial release.
--- R/act_tbl.R
+++ R/act_tbl.R
@@ -80,8 +80,8 @@ is_act_tbl <- function(act_tbl) {
 #' @return \code{summary.act_tbl} returns a tibble with a single row,
 #'         containing a summary of the given \code{act_tbl}.
 #' @importFrom dplyr lag mutate pull slice
-#' @importFrom lubridate as.duration
-#' @importFrom timetk slidify
+#' @importFrom lubridate as.duration dseconds
+#' @importFrom slider slide_index_dbl
 #' @importFrom tibble tribble
 #' @rdname act_tbl
 summary.act_tbl <- function(object,
@@ -158,15 +158,16 @@ summary.act_tbl <- function(object,
   # If we have elevation, add AvgElev, ElevGain and ElevLoss
   if ("ele" %in% colnames(object)) {
     # We use a rolling 30 second average of elevation to smooth out jitter.
-    period <- min(30, nrow(object))
+    period <- dseconds(min(15, nrow(object)))
     elev_df <- object %>%
       mutate(
-        ele = slidify(
+        ele = slide_index_dbl(
+          .data$ele,
+          .data$time,
           mean,
-          .period = period,
-          .align = "center",
-          .partial = TRUE
-        )(.data$ele)
+          .before = period,
+          .after = period
+        )
       ) %>%
       mutate(
         elevdelta = .data$ele - lag(.data$ele),
